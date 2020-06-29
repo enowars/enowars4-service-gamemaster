dbf8b08942e2e4c8f97da73f7d97e9cac9c5a8db
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2020-06-29 21:06:48 +0200

    Correctly skip the wrong batches

--- checker/GamemasterChecker/GamemasterSignalR.cs
+++ checker/GamemasterChecker/GamemasterSignalR.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -49,7 +50,10 @@ namespace GamemasterChecker
                 if (ContentToCompare != null)
                 {
                     if (MessageBatchIndex != 0)
+                    {
                         MessageBatchIndex -= 1;
+                        return;
+                    }
                     Logger.LogDebug($"Comparing {messages.Count()} messages to: {ContentToCompare}");
                     foreach (var e in messages)
                     {
