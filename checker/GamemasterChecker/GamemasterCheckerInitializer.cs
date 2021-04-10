980a222116b381e19da1ac40520ae9766dc958b1
Benedikt Radtke <benediktradtke@gmail.com>
2021-04-10 11:51:15 +0200

    signalrclients

--- checker/GamemasterChecker/GamemasterCheckerInitializer.cs
+++ checker/GamemasterChecker/GamemasterCheckerInitializer.cs
@@ -33,6 +33,7 @@
                     }
                 });
             collection.AddTransient(typeof(GamemasterClient));
+            collection.AddTransient(typeof(GamemasterSignalRClient));
         }
     }
 }
